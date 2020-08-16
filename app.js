c032eca48b93727cb95d4c45179ad8889cc255a1
Philippe_Lanougadere <p.lanougadere@gmail.com>
2020-08-16 19:47:36 +0200

    add delete functionality

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-// To use environment variables in config.env
-dotenv.config({ path: "./config.env" });
-
+// CONFIGURATION EXPRESS //
 const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// CONNECTION TO MONGODB //
+dotenv.config({ path: "./config.env" }); // To use environment variables in config.env
 
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
@@ -18,38 +23,33 @@ mongoose.connect(DB, {
   useUnifiedTopology: true,
 });
 
+// CREATING MODEL WITH MONGOOSE //
 const taskSchema = {
   name: String,
 };
 
 const Task = mongoose.model("Task", taskSchema);
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-let task_tab = [];
-
+// CREATING ROUTES //
 app.get("/", (req, res) => {
-  res.render("index", { tasks: task_tab });
-  console.log(process.env.DATABASE);
+  Task.find((err, tasks) => {
+    res.render("index", { tasks: tasks });
+  });
 });
 
 app.post("/", (req, res) => {
   const task = new Task({
     name: req.body.task,
   });
-  task_tab.push(task.name);
   task.save();
-
   res.redirect("/");
 });
 
 app.get("/delete/:id", (req, res) => {
-  const found_index = task_tab.findIndex((item) => item === req.params.id);
-  console.log(found_index);
-  task_tab.splice(found_index, 1);
-  res.redirect("/");
+  Task.deleteOne({ name: req.params.id }, (err) => {
+    if (err) console.log(err);
+    else res.redirect("/");
+  });
 });
 
 app.listen(8080, () => console.log("server working on 8080"));
