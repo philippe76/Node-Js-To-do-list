13cb58e3926a1db4fa0c89073ed09e867e1761f5
Philippe_Lanougadere <p.lanougadere@gmail.com>
2020-08-16 18:21:39 +0200

    add connection to mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// To use environment variables in config.env
+dotenv.config({ path: "./config.env" });
 
 const app = express();
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const taskSchema = {
+  name: String,
+};
+
+const Task = mongoose.model("Task", taskSchema);
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,10 +32,16 @@ let task_tab = [];
 
 app.get("/", (req, res) => {
   res.render("index", { tasks: task_tab });
+  console.log(process.env.DATABASE);
 });
 
 app.post("/", (req, res) => {
-  task_tab.push(req.body.task);
+  const task = new Task({
+    name: req.body.task,
+  });
+  task_tab.push(task.name);
+  task.save();
+
   res.redirect("/");
 });
 
